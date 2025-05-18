ed09f53ac8530a2cc34477c8576dde34f9dee68d
roshniandy <roshniandy@gmail.com>
2025-05-18 20:45:30 +1000

    sqlite connection string

--- Vypex.CodingChallenge.Service/Vypex.CodingChallenge.Infrastructure/InfrastructureModule.cs
+++ Vypex.CodingChallenge.Service/Vypex.CodingChallenge.Infrastructure/InfrastructureModule.cs
@@ -11,18 +11,12 @@ namespace Vypex.CodingChallenge.Infrastructure
     {
         public static IServiceCollection AddInfrastructureModule(this IServiceCollection services, IConfiguration configuration)
         {
-            //services.AddDbContext<AppDbContext>(options =>
-            //    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<AppDbContext>(options =>
             {
-                //     options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=TestApiDb;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true")
-                //.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                //options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnectionSqlLite"))
                .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
             });
-           
-            //services.AddDbContext<CodingChallengeContext>(options => options
-            //    .UseSqlite(connectionString));
 
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             services.AddScoped<ILeaveRepository, LeaveRepository>();
